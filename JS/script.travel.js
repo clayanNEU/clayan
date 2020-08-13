abc2d72e94ceb9902b722dc2f99ac539af7965d2
Clayton Yan <yan.cl@northeastern.edu>
2020-08-13 15:53:30 -0700

    get that js transition

--- JS/script.travel.js
+++ JS/script.travel.js
@@ -2,3 +2,16 @@
 function underConstruction() {
   alert("Under Construction :), please check out my resume or blog!!");
 }
+
+const checkpoint = 1000;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= checkpoint) {
+    opacity = 1 - currentScroll / checkpoint;
+  } else {
+    opacity = 0;
+  }
+  document.querySelector(".front").style.opacity = opacity;
+  console.log(opacity);
+});
