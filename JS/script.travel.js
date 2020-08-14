157fcae929096120a8f530ccb2dd6c20784d0bc2
Clayton Yan <yan.cl@northeastern.edu>
2020-08-13 18:16:41 -0700

    added content for post 8-13

--- JS/script.travel.js
+++ JS/script.travel.js
@@ -3,11 +3,13 @@ function underConstruction() {
   alert("Under Construction :), please check out my resume or blog!!");
 }
 
+// set constant for transitioning of sushi images
 const checkpoint = 900;
 
 window.addEventListener("scroll", () => {
   const currentScroll = window.pageYOffset;
   if (currentScroll <= checkpoint) {
+    // adjusting opacity based on how far you scroll
     opacity = 1 - currentScroll / checkpoint + 0.2;
   } else {
     opacity = 0;
